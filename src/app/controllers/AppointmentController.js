dba55b4a5dfef6d0740ba9fe79d9cc743e1421a7
felipefln <felipecarvalho_22_@hotmail.com>
2019-11-19 09:26:07 -0300

    Feature: Adicionado paginação nos agendamentos e a relação entre agendamento e avatar

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,16 +1,45 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import User from '../models/Users';
+import File from '../models/File';
 import Appointment from '../models/Appointment';
 
 class AppointmentController {
+    async index(req, res) {
+        const { page = 1 } = req.query;
+
+        const appointments = await Appointment.findAll({
+            where: { user_id: req.userId, canceled_at: null },
+            order: ['date'],
+            attributes: ['id', 'date'],
+            limit: 20,
+            offset: (page - 1) * 20,
+            include: [
+                {
+                    model: User,
+                    as: 'provider',
+                    attributes: ['id', 'name'],
+                    include: [
+                        {
+                            model: File,
+                            as: 'avatar',
+                            attributes: ['id', 'path', 'url'],
+                        },
+                    ],
+                },
+            ],
+        });
+
+        return res.json(appointments);
+    }
+
     async store(req, res) {
         const schema = Yup.object().shape({
             provider_id: Yup.number().required(),
             date: Yup.date().required(),
         });
-
         if (!(await schema.isValid(req.body))) {
-            return res.status(400).json({ error: 'Validação appointmet' });
+            return res.status(400).json({ error: 'Validação agendamento' });
         }
 
         const { provider_id, date } = req.body;
@@ -21,6 +50,26 @@ class AppointmentController {
 
         if (!isProvider) {
             return res.status(401).json({ error: 'Não és Provider' });
+        }
+
+        const hourStart = startOfHour(parseISO(date));
+
+        if (isBefore(hourStart, new Date())) {
+            return res.status(400).json({ error: 'Ja passou essa data' });
+        }
+
+        const checkPossibility = await Appointment.findOne({
+            where: {
+                provider_id,
+                canceled_at: null,
+                date: hourStart,
+            },
+        });
+
+        if (checkPossibility) {
+            return res
+                .status(400)
+                .json({ error: 'Essa data já tem agendamento' });
         }
 
         const appointmet = await Appointment.create({
